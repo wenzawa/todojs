15007cccfd55fb5f5af47145aa8622dd48c6791b
Christian SHUNGU <christianshungu@gmail.com>
2021-11-22 21:55:09 +0100

    Initialisation du projet Todo en javascript

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+/**
+    * @description      : 
+    * @author           : christian
+    * @group            : 
+    * @created          : 07/10/2021 - 21:00:26
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 07/10/2021
+    * - Author          : christian
+    * - Modification    : 
+**/
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main : path.join(__dirname, 'src/app.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {loader: "babel-loader"}
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "postcss-loader"},
+                    {loader: "sass-loader"}
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {loader: "file-loader"}
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html")
+        })
+    ],
+    devtool: "source-map",
+    mode: "development",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 4000
+    }
+};
